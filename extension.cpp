3fe6cd1398cce0f617eafa94e40253eee7057396
fakuivan <fakuivan@gmail.com>
2018-06-18 20:22:40 -0300

    Implement POC

--- extension.cpp
+++ extension.cpp
@@ -30,12 +30,70 @@
  */
 
 #include "extension.h"
+#include "KeyValues.h"
 
 /**
  * @file extension.cpp
  * @brief Implement extension code here.
  */
 
+#define CONFIG_FILE_NAME	"tf2sii"
+#define CONFIG_FILE_KV		"CTFItemSchema::m_pKeyValues"
+#define CONFIG_FILE_GII		"GetItemSchema"
+
 TF2SimpleItemInfo g_TF2SimpleItemInfo;		/**< Global singleton for extension's main interface */
 
 SMEXT_LINK(&g_TF2SimpleItemInfo);
+
+struct {
+	typedef void* (*func)();
+	int nKvOffset;
+	void* pGetItemSchema;
+	bool bReady;
+	KeyValues * operator()() {
+		if (!bReady) { return NULL; }
+		auto pItemSchema = ((func)pGetItemSchema)();
+		if (pItemSchema == NULL) { return NULL; }
+		return *((KeyValues **)((char*)(pItemSchema) + nKvOffset));
+	}
+} GetItemSchemaKV;
+
+bool TF2SimpleItemInfo::SDK_OnLoad(char * error, size_t maxlength, bool late)
+{
+	GetItemSchemaKV.bReady = false;
+	IGameConfig *gameConfig = NULL;
+	auto configError = new char[maxlength];
+	if (!gameconfs->LoadGameConfigFile(CONFIG_FILE_NAME, &gameConfig, configError, maxlength))
+	{
+		snprintf(error, maxlength, "Failed to read config file '" CONFIG_FILE_NAME "': %s\n", configError);
+		return false;
+	}
+	delete configError;
+
+	if (!gameConfig->GetMemSig(CONFIG_FILE_GII, &(GetItemSchemaKV.pGetItemSchema)))
+	{
+		snprintf(error, maxlength, "Failed to find section '" CONFIG_FILE_GII "' for signature\n");
+		return false;
+	}
+	if (GetItemSchemaKV.pGetItemSchema == NULL)
+	{
+		snprintf(error, maxlength, "Failed to find location on memory for signature " CONFIG_FILE_GII "\n");
+		return false;
+	}
+	
+	if (!gameConfig->GetOffset(CONFIG_FILE_KV, &(GetItemSchemaKV.nKvOffset)))
+	{
+		snprintf(error, maxlength, "Failed to find section '" CONFIG_FILE_KV "' for offset\n");
+		return false;
+	}
+	
+	GetItemSchemaKV.bReady = true;
+	auto pItemSchemaKV = GetItemSchemaKV();
+	if (pItemSchemaKV == NULL)
+	{
+		snprintf(error, maxlength, "Failed to get property 'CTFItemSchema::m_pKeyValues'\n");
+		return false;
+	}
+	g_pSM->LogMessage(myself, "Section name for item schema: '%s'.", pItemSchemaKV->GetName());
+	return true;
+}
